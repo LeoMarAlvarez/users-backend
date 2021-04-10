980c0f4303255bc64064047e4d62e65d7e0c41fd
LEO <alvarezleom@gmail.com>
2021-04-10 09:50:35 -0300

    creation Usuario model -m

---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Usuario;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UsuarioFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Usuario::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->name,
+            'apellido' => $this->faker->lastName,
+            'usuario' => $this->faker->unique()->name,
+            'email' => $this->faker->unique()->safeEmail,
+        ];
+    }
+}
